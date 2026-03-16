ae9976721b1906c61168ef27774279f5625f5ef0
Martin de Larminat <mcdelarm@email.unc.edu>
2026-03-16 14:53:16 -0400

    Project Initialization

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import {Routes, Route, Navigate} from 'react-router-dom';
+import FlashCardPage from './pages/FlashCardPage';
+import LandingPage from './pages/LandingPage';
+import HighlightsPage from './pages/HighlightsPage';
+import Header from './components/Header';
+import './styles/globals.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/flashcards' element={<FlashCardPage/>}/>
+        <Route path='/highlights' element={<HighlightsPage/>}/>
+        <Route path='*' element={<Navigate to='/'/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/HighlightsPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HighlightsPage = () => {
+  return (
+    <div>HighlightsPage</div>
+  )
+}
+
+export default HighlightsPage
