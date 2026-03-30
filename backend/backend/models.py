7638d9ada1e021ff25240a0bf939ae75675870d7
Martin de Larminat <mcdelarm@email.unc.edu>
2026-03-30 15:22:12 -0400

    Updated postgre models

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -1,47 +1,55 @@
-# from sqlalchemy import Column, Integer, String, Text, ForeignKey, TIMESTAMP, Boolean
-# from sqlalchemy.orm import relationship
-# from .database import Base
-# from datetime import datetime
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, TIMESTAMP, Boolean, UniqueConstraint
+from sqlalchemy.orm import relationship
+from .database import Base
+from datetime import datetime
 
-# class User(Base):
-#   __tablename__ = 'users'
+class User(Base):
+  __tablename__ = 'users'
 
-#   id = Column(Integer, primary_key=True, index=True)
-#   username = Column(String(50), unique=True, nullable=False)
-#   email = Column(String(150), unique=True, nullable=False)
-#   hashed_password = Column(Text, nullable=False)
-#   created_at = Column(TIMESTAMP, default=datetime.utcnow)
+  id = Column(Integer, primary_key=True, index=True)
+  email = Column(String(150), unique=True, nullable=False)
+  hashed_password = Column(Text, nullable=False)
+  created_at = Column(TIMESTAMP, default=datetime.utcnow)
+  flashcards = relationship('Flashcard', back_populates='owner', cascade='all, delete')
+  highlights = relationship('Highlight', back_populates='owner', cascade='all, delete')
 
-#   flashcards = relationship('Flashcard', back_populates='owner', cascade='all, delete')
-#   highlights = relationship('Highlight', back_populates='owner', cascade='all, delete')
-#   books = relationship('Book', back_populates='owner', cascade='all, delete')
 
-# class Book(Base):
-#   __tablename__ = 'books'
+class Book(Base):
+  __tablename__ = 'books'
+  __table_args__ = (UniqueConstraint('title', 'author', name='uq_books_title_author'),)
 
-#   id = Column(Integer, primary_key=True, index=True)
-#   user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'))
-#   title = Column(String(255), nullable=False)
-#   author = Column(String(255), nullable=True)
-#   created_at = Column(TIMESTAMP, default=datetime.utcnow)
+  id = Column(Integer, primary_key=True, index=True)
+  title = Column(String(255), nullable=False)
+  author = Column(String(255), nullable=False)
+  created_at = Column(TIMESTAMP, default=datetime.utcnow)
 
-#   owner = relationship('User', back_populates='books')
-#   flashcards = relationship('Flashcard', back_populates='book', cascade='all, delete')
-#   highlights = relationship('Highlight', back_populates='book', cascade='all, delete')
 
-# class Flashcard(Base):
-#   __tablename__ = 'flashcards'
+class Flashcard(Base):
+  __tablename__ = 'flashcards'
 
-#   id = Column(Integer, primary_key=True, index=True)
-#   user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'))
-#   word = Column(String(255), nullable=False)
-#   definition = Column(Text, nullable=False)
-#   context_sentence = Column(Text, nullable=True)
-#   created_at = Column(TIMESTAMP, default=datetime.utcnow)
-#   book_id = Column(Integer, ForeignKey('books.id', ondelete='CASCADE'))
-#   known = Column(Boolean, default=False) 
-#   location = Column(String(255), nullable=True)
-#   part_of_speech = Column(String(50), nullable=True)
+  id = Column(Integer, primary_key=True, index=True)
+  user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'))
+  book_id = Column(Integer, ForeignKey('books.id', ondelete='CASCADE'))
+  stem = Column(String(255), nullable=False)
+  word = Column(String(255), nullable=False)
+  definition = Column(Text, nullable=False)
+  context = Column(Text, nullable=True)
+  created_at = Column(TIMESTAMP, default=datetime.utcnow)
+  known = Column(Boolean, default=False) 
+  part_of_speech = Column(String(50), nullable=False)
+  owner = relationship('User', back_populates='flashcards')
+  book = relationship('Book')
 
-#   owner = relationship('User', back_populates='flashcards')
-#   book = relationship('Book', back_populates='flashcards')
+
+class Highlight(Base):
+  __tablename__ = 'highlights'
+
+  id = Column(Integer, primary_key=True, index=True)
+  user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'))
+  book_id = Column(Integer, ForeignKey('books.id', ondelete='CASCADE'))
+  text = Column(Text, nullable=False)
+  location = Column(String(255), nullable=True)
+  created_at = Column(TIMESTAMP, default=datetime.utcnow)
+  starred = Column(Boolean, default=False) 
+  owner = relationship('User', back_populates='highlights')
+  book = relationship('Book')
